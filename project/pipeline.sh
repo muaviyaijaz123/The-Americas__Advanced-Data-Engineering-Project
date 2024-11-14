d55056dbbecd28269d18963f2faca68d972c00e6
Muaviya Ijaz <muaviyaijaz@Muaviyas-MBP.fritz.box>
2024-11-14 14:13:38 +0100

    Code resolved

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 python3 ./project/pipeline.py
 
+#Requirements to run the pipeline
 # 1. Please sign into your Kaggle Accont first
 # 2. Go to Settings option from top right
 # 3. Create 'New API token' which will crete 'kaggle.json' file
 # 4. Place this kaggle.json file into project folder and then run the pipeline
+
+#5. Install the following imports before runnning the pipeline. Later will add it in a separate file
+
+# import shutil
+# import platform
+# import subprocess
+# import zipfile
+# import pandas as pd
+# import sqlite3
